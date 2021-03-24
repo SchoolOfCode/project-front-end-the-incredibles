c5de0d1f121f61ecea0bd383b993d0cee26ab05a
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-03-24 16:18:31 +0000

    styling homepage coauthored liam

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import CallToAction from "../CallToAction";
 import BusinessNavBar from "../NavBar/BusinessNavBar";
+import Footer from "../Footer";
+
 import styles from "./HomePage.module.css";
 
 function HomePage() {
@@ -13,11 +15,12 @@ function HomePage() {
         </div>
         <div className={styles.feature}>
           <img
-            src="https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
+            src="https://images.unsplash.com/photo-1551028150-64b9f398f678?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80"
             alt="demo"
           />
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
