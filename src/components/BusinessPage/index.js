c911edfa8b89404b0f37ea0ff3be3615660b9004
Liam Morley <liammorley123@live.co.uk>
2021-03-25 16:20:54 +0000

    add to businesspage CSS - coauthor: nathan

--- src/components/BusinessPage/index.js
+++ src/components/BusinessPage/index.js
@@ -1,27 +1,35 @@
-import React from 'react';
-import mockData from '../../../libs/mockData';
-import BusinessProfile from '../BusinessProfile';
-import ProductCard from "../ProductCard"
+import React from "react";
+import mockData from "../../libs/mockData";
+import BusinessProfile from "../BusinessProfile";
+import ProductCard from "../ProductCard";
+import "../../index.css";
 
-import {container, profileContainer, cardContainer} from "./BusinessPage.module.css"
+import {
+  container,
+  profileContainer,
+  cardContainer,
+} from "./BusinessPage.module.css";
 
 function BusinessPage() {
-	const { name, about, img, isTrading, products } = mockData;
+  const { name, about, img, isTrading, products } = mockData;
 
-	return (
-		<div className={container}>
-			<div className={profileContainer}>
-				<BusinessProfile
-					name={name}
-					about={about}
-					img={img}
-					isTrading={isTrading}
-				/>
-			</div>
-			<div className={cardContainer}
-            >{products.map((product, i) => <ProductCard key={i}/>)}</div>
-		</div>
-	);
+  return (
+    <div className={container}>
+      <div className={profileContainer}>
+        <BusinessProfile
+          name={name}
+          about={about}
+          img={img}
+          isTrading={isTrading}
+        />
+      </div>
+      <div className={cardContainer}>
+        {products.map((product, i) => (
+          <ProductCard key={i} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default BusinessPage;
