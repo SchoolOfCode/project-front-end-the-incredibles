bd69d393405ba8fe3e885aac4d66aed54212c007
Liam Morley <liammorley123@live.co.uk>
2021-03-25 15:53:43 +0000

    add out of stock to card

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -3,9 +3,10 @@ import styles from "./ProductCard.module.css";
 import AddtoCartButton from "../Buttons/AddToCartButton";
 import QuantityInput from "../Inputs/NumberInput";
 
-function ProductCard() {
+function ProductCard({ InventoryQuantity = 0 }) {
+  const inStockClass = InventoryQuantity ? "Stocked" : "OutOfStock";
   return (
-    <div className={styles.ContentFlex}>
+    <div className={`${styles.ContentFlex} ${styles[inStockClass]}`}>
       <div className={styles.innerContent}>
         <img
           src="https://images.unsplash.com/photo-1590069832258-a222a34722c3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
@@ -14,7 +15,7 @@ function ProductCard() {
         <p>Macrame</p>
         <p>£2000</p>
         <QuantityInput />
-        <AddtoCartButton />
+        {InventoryQuantity ? <AddtoCartButton /> : <p>Out of stock</p>}
       </div>
     </div>
   );
