6280115c012901ba856ee6ab274f452f3766f041
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-03-25 10:50:41 +0000

    nav started

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,62 @@
-import HomePage from "../HomePage";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import "./App.css";
+import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	NavLink,
+	Route,
+	Switch,
+} from 'react-router-dom';
+import '../../index.css';
+import SignInUpButton from '../Buttons/SignInUpButton';
+import SignOutButton from '../Buttons/SignOutButton';
+import HomePage from '../HomePage';
+import css from '../NavBar/NavBar.module.css';
+import './App.css';
 
 function App() {
-  return (
-    <div>
-      <HomePage/>
-    </div>
-  );
+	const { isAuthenticated } = useAuth0();
+	return (
+		<Router>
+			<div className='App'>
+				<nav className='nav-bar'>
+					<h1>www.veg.com</h1>
+
+					<ul>
+						<li>
+							<NavLink to='/' activeClassName='active' inactiveClassName='inactive'>
+								Home
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/about' activeClassName='active'
+              inactiveClassName='inactive'>
+								About
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/ace-ventura' activeClassName='active'>
+								Ace Ventura
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
+
+				{/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+				<Switch>
+					<Route path='/ace-ventura'>
+						<p>one</p>
+					</Route>
+					<Route path='/about'>
+						<p>two</p>
+					</Route>
+					<Route path='/'>
+						<p>three</p>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/NavBar/BusinessNavBar/index.js
+++ src/components/NavBar/BusinessNavBar/index.js
@@ -1,50 +1,79 @@
-import React from "react";
-import css from "../NavBar.module.css";
-import {useAuth0} from "@auth0/auth0-react";
-
-// import { Link } from "react-router-dom";
-import "../../../index.css";
-import SignInUpButton from "../../Buttons/SignInUpButton";
-import SignOutButton from "../../Buttons/SignOutButton";
+import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	NavLink,
+	Route,
+	Switch,
+} from 'react-router-dom';
+import '../../../index.css';
+import SignInUpButton from '../../Buttons/SignInUpButton';
+import SignOutButton from '../../Buttons/SignOutButton';
+import HomePage from '../../HomePage';
+import css from '../NavBar.module.css';
 
 function BusinessNavBar(props) {
-  const {isAuthenticated} = useAuth0()
-  
-  return (
-    <div className={css.navFlex}>
-      <div className={css.innerFlex}>
-        <div>
-          <h1> CartShop</h1>
-        </div>
-        <div className={css.linkFlex}>
-          {/* <Link className={css.links} to={`/`}>
+	const { isAuthenticated } = useAuth0();
+
+	return (
+		<Router>
+			<div className={css.navFlex}>
+				<div className={css.innerFlex}>
+					<div>
+						<h1> CartShop</h1>
+					</div>
+					<nav className={css.linkFlex}>
+						{/* <Link className={css.links} to={`/`}>
             My Cartshop
-          </Link>
-          <Link className={css.links} to={`/`}>
+            </Link>
+            <Link className={css.links} to={`/`}>
             About
-          </Link>
-          <Link className={css.links} to={`/`}>
+            </Link>
+            <Link className={css.links} to={`/`}>
             Tutorial
           </Link> */}
-          <a href="#" className={css.links}>
-            My CartShop
-          </a>
-          <a href="#" className={css.links}>
-            About
-          </a>
-          <a href="#" className={css.links}>
-            Tutorial
-          </a>
-
-        </div>
-          {isAuthenticated ? 
-          <SignOutButton />
-          :
-          <SignInUpButton textContent="Sign in/up"/>
-          }
-      </div>
-    </div>
-  );
+						<NavLink
+							to='/'
+							className={css.links}
+							activeClassName={css.active}
+						>
+							My CartShop
+						</NavLink>
+						<NavLink
+							to='/about'
+							className={css.links}
+							activeClassName={css.active}
+						>
+							About
+						</NavLink>
+						<NavLink
+							to='/tutorial'
+							className={css.links}
+							activeClassName={css.active}
+						>
+							Tutorial
+						</NavLink>
+					</nav>
+					{isAuthenticated ? (
+						<SignOutButton />
+					) : (
+						<SignInUpButton textContent='Sign in/up' />
+					)}
+				</div>
+				<Switch>
+					<Route path='/about'>
+						<p>ABOUT PAGE WOW</p>
+					</Route>
+					<Route path='/users'>
+						<p>TUTORIAL WOW</p>
+					</Route>
+					<Route path='/'>
+						<HomePage />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default BusinessNavBar;
