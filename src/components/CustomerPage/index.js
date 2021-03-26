de2dbf88c2f4c78c299fb5b0d6e7d6bec1cd93c7
Liam Morley <liammorley123@live.co.uk>
2021-03-26 12:12:14 +0000

    updated customer page with data

---
+++ src/components/CustomerPage/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Button from "../Buttons/Button";
+import BusinessProfile from "../BusinessProfile";
+import ProductCard from "../ProductCard";
+import mockData from "../../libs/mockData.js";
+
+import {
+  container,
+  profileContainer,
+  cardContainer,
+} from "./CustomerPage.module.css";
+
+function CustomerPage() {
+  const {
+    businessName,
+    businessAbout,
+    businessImg,
+    isTrading,
+    products,
+  } = mockData;
+
+  return (
+    <div className={container}>
+      <div className={profileContainer}>
+        <BusinessProfile
+          name={businessName}
+          about={businessAbout}
+          img={businessImg}
+          isTrading={isTrading}
+        />
+      </div>
+      <div className={cardContainer}>
+        {products.map((product, i) => (
+          <ProductCard
+            name={product.name}
+            price={product.price}
+            key={i}
+            inventoryQuantity={product.inventoryQuantity}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default CustomerPage;
--- src/components/NavBar/CustomerNavBar/index.js
+++ src/components/NavBar/CustomerNavBar/index.js
@@ -1,44 +1,40 @@
-import React from 'react';
+import React from "react";
+import CustomerPage from "../../CustomerPage";
+import Button from "../../Buttons/Button";
 import {
-	BrowserRouter as Router,
-	NavLink,
-	Route,
-	Switch,
-} from 'react-router-dom';
-import '../../../index.css';
-import css from '../NavBar.module.css';
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch,
+} from "react-router-dom";
+import "../../../index.css";
+import css from "../NavBar.module.css";
 
 function CustomerNavBar(props) {
-	return (
-		<Router>
-			<div className={css.navFlex}>
-				<nav className={css.linkFlex}>
-					<h1> CartShop</h1>
-					<NavLink
-						to={'/store'}
-						exact
-						className={css.links}
-						activeClassName={css.active}
-					>
-						Pete the Meat
-					</NavLink>
-					<NavLink
-						to={'/basket'}
-						exact
-						className={css.links}
-						activeClassName={css.active}
-					>
-						Basket
-					</NavLink>
-				</nav>
-			</div>
-			<Switch>
-				<Route path='/store'>
-					<p>The store cartShop would be here</p>
-				</Route>
-			</Switch>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className={css.navFlex}>
+        <nav className={css.linkFlex}>
+          <h1> CartShop</h1>
+          <NavLink
+            to={"/store"}
+            exact
+            className={css.links}
+            activeClassName={css.active}
+          >
+            Macrambé Master
+          </NavLink>
+
+          <Button textContent={"basket"}></Button>
+        </nav>
+      </div>
+      <Switch>
+        <Route path="/store">
+          <CustomerPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default CustomerNavBar;
