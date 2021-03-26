4975484a2954c761b73b1291261e11384d4abb6e
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-03-26 19:44:59 +0000

    mock data improved

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -11,8 +11,8 @@ function ProductCard({ product, onClick }) {
     <div className={`${styles.ContentFlex} ${styles[inStockClass]}`}>
       <div className={styles.innerContent}>
         <img
-          src="https://images.unsplash.com/photo-1590069832258-a222a34722c3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-          alt=""
+          src={product.src}
+          alt={product.alt}
         />
         <p className={styles.name}>{product.productName}</p>
         <p className={styles.price}>£{product.price}</p>
--- src/libs/mockData.js
+++ src/libs/mockData.js
@@ -1,6 +1,6 @@
 const mockBusiness = {
-  businessName: "Macrambe Master",
-  businessAbout: "We make really epic macrambe things",
+  businessName: "Macramè Master",
+  businessAbout: "We make really epic macramè things",
   businessImg: {
     src:
       "https://images.unsplash.com/photo-1535295972055-1c762f4483e5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
@@ -9,37 +9,61 @@ const mockBusiness = {
   isTrading: true,
   products: [
     {
-      productName: "one",
+      productName: "White Macramè",
+      img: {
+        src: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mylittlewish.com%2Fproducts%2Fboho-macrame-wall-hanging-xl&psig=AOvVaw3GJ__oZ-3H_9dZI3b745Dg&ust=1616874006297000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCOiprbzu8CFQAAAAAdAAAAABAD",
+        alt: "white macrame"
+      },
       price: 2.56,
       inventoryQuantity: 4,
       productId: 1,
       qty: 0
     },
     {
-      productName: "two",
+      productName: "Leaf Macramè",
+       img: {
+        src: "https://honestlywtf.com/wp-content/uploads/2018/10/macramefeather30-2.jpg",
+        alt: "leaf macramè"
+      },
       price: 2.56,
       inventoryQuantity: 0,
       productId: 2,
     },
     {
-      productName: "three",
+     productName: "Plant Macramè",
+       img: {
+        src: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thesprucecrafts.com%2Fbeginner-plant-hanger-patterns-4768729&psig=AOvVaw3ASYgUgb4N4XLpvY_xjbt3&ust=1616874115039000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNih6tPbzu8CFQAAAAAdAAAAABAD",
+        alt: "Plant macramè"
+      },
       price: 2.56,
       inventoryQuantity: 3,
       productId: 3,
     },
     {
-      productName: "one",
+      productName: "Dog Macramè",
+       img: {
+        src: "https://i.pinimg.com/736x/7c/d4/db/7cd4dbbe1932bcabaf5c433fff435690.jpg",
+        alt: "Dog macramè"
+      },
       price: 2.56,
       inventoryQuantity: 4,
       productId: 4,
     },
     {
-      productName: "two",
+      productName: "Poodle Macramè",
+       img: {
+        src: "https://i.ebayimg.com/images/g/DikAAOSwaZRdSy88/s-l300.jpg",
+        alt: "Poodle macramè"
+      },
       price: 2.56,
       inventoryQuantity: 0,
     },
     {
-      productName: "three",
+      productName: "Curtain Macramè",
+       img: {
+        src: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F420382946469180378%2F&psig=AOvVaw3AUh-Xy-d-dNdsLpsI_NFg&ust=1616874227032000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNiphILczu8CFQAAAAAdAAAAABAD",
+        alt: "Curtain macramè"
+      },
       price: 2.56,
       inventoryQuantity: 3,
       productId: 5,
