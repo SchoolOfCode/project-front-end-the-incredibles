02dce50227d5555707d94173e81120949853031e
Rob <robhodgskin@gmail.com>
2021-03-29 16:26:44 +0100

    stripe checkout

--- src/components/Basket/index.js
+++ src/components/Basket/index.js
@@ -1,8 +1,12 @@
 import CheckoutButton from "../Buttons/CheckoutButton";
 import ProductInBasket from "../ProductInBasket";
 import { basket } from "./Basket.module.css";
+import Stripe from "../Stripe";
 
 function Basket({ cartItems, onRemoveAll, onRemoveOne, onAdd }) {
+  let count = 0;
+  cartItems.map((item)=>{count+=item.price})
+
   return (
     <div className={`${basket} basket`}>
       <h2>Basket</h2>
@@ -16,7 +20,7 @@ function Basket({ cartItems, onRemoveAll, onRemoveOne, onAdd }) {
             onAdd={onAdd}
           />
         ))}
-      <CheckoutButton onClick={() => console.log(cartItems)} />
+      <CheckoutButton onClick={Stripe.handleClick} />
     </div>
   );
 }
