62a054943c1a66b2ecd27bef1792560d583d9938
sara <sara97@live.co.uk>
2021-03-30 14:35:38 +0100

    delete product method added

--- src/components/CurrentPoductsList/index.js
+++ src/components/CurrentPoductsList/index.js
@@ -3,6 +3,11 @@ import Button from "../Buttons/Button";
 import {currentProductsContainer, currentProduct} from "./CurrentProductList.module.css";
 
 function CurrentProductList({products,removeProduct}) {
+
+  async function deleteProduct(id){
+    await fetch(`${process.env.REACT_APP_BACKEND_URL}/business/deletebyproduct/${id}`, {method:'DELETE', headers:  { 'Content-Type' : 'application/json' }})
+  }
+
     return (
         <ul className={currentProductsContainer}>
           <h2>Current Products:</h2>
@@ -10,7 +15,8 @@ function CurrentProductList({products,removeProduct}) {
             <li key={i} className={currentProduct}>
               <span> {product.productName}</span>
               <span> £{product.productPrice}</span>
-              <Button className="removeProduct" textContent="remove" onClick={() => removeProduct(i)} />
+              <Button className="removeProduct" textContent="remove" onClick={() => {removeProduct(i)
+              deleteProduct(product.productId)}} />
             </li>
           ))}
         </ul>
