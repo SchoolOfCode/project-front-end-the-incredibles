fb10f5e358648df68d45f9c8a7708dbafb1d2ce8
Rob <robhodgskin@gmail.com>
2021-03-30 13:40:53 +0100

    youtube tutorial

--- src/components/Tutorial/index.js
+++ src/components/Tutorial/index.js
@@ -1,4 +1,6 @@
 import {tutorial, heading, subheading, introduction, mission} from "./Tutorial.module.css"
+import YouTube from "@u-wave/react-youtube";
+import { getId } from "../../libs/index";
 
 function Tutorial() {
 	return (
@@ -8,6 +10,9 @@ function Tutorial() {
 				<h2 className={subheading}>This Site is Easy</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <p>nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam id leo in vitae turpis massa sed elementum tempus egestas sed sed risus pretium</p>
+			</section>
+			<section>
+			<YouTube video={getId("https://youtu.be/di4KLmuvOxA")}/>
 			</section>
             <section className={mission}>
 			<h2 className={subheading}>Another Heading</h2>
