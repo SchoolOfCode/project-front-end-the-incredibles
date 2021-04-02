632142f9a1e36513665de418b24673410a9b9338
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-04-02 16:13:38 +0100

    link to git team

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,18 @@
-import React from "react";
-import styles from "./Footer.module.css";
-import "../../index.css";
+import React from 'react';
+import '../../index.css';
+import styles from './Footer.module.css';
 
-function Footer({ textContent = "made with love in birmingham" }) {
-  return (
-    <div className={styles.footerFlex}>
-      <div className={styles.contentFlex}>
-        <p>{textContent}</p>
-        <i class="fab fa-github"></i>
-      </div>
-    </div>
-  );
+function Footer({ textContent = 'made with love in birmingham' }) {
+	return (
+		<div className={styles.footerFlex}>
+			<div className={styles.contentFlex}>
+				<p>{textContent}</p>
+				<a href='https://github.com/orgs/SchoolOfCode/teams/the-incredibles'>
+					<i class='fab fa-github' />
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
