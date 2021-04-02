d2c6cc9c3839498a1c183dc05a37b58295e5c8e0
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-04-02 21:15:04 +0100

    update product form styled

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,89 +1,97 @@
-import { useState } from "react";
-import Button from "../Buttons/Button/index";
+import { useState } from 'react';
+import Button from '../Buttons/Button/index';
+import Input from '../Inputs/Input';
 import {
-  currentProduct
-} from "./Product.module.css";
-import Input from "../Inputs/Input";
+	currentProduct,
+	currentProductForm,
+	currentProductInfo,
+} from './Product.module.css';
 
 export default function Product({ product, removeProduct, deleteProduct }) {
-  const [canEditProduct, setCanEditProduct] = useState(false);
+	const [ canEditProduct, setCanEditProduct ] = useState(false);
 
-  function toggleCanEdit() {
-    setCanEditProduct(!canEditProduct);
-  }
+	function toggleCanEdit() {
+		setCanEditProduct(!canEditProduct);
+	}
 
-  async function editProduct(id) {
-    await fetch(
-      `${process.env.REACT_APP_BACKEND_URL}/business/updatebyproduct/${id}`,
-      {
-        method: "PUT",
-        body: JSON.stringify({ ...productData }),
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-  }
-  const [productData, setProductData] = useState({ ...product });
+	async function editProduct(id) {
+		await fetch(
+			`${process.env
+				.REACT_APP_BACKEND_URL}/business/updatebyproduct/${id}`,
+			{
+				method  : 'PUT',
+				body    : JSON.stringify({ ...productData }),
+				headers : { 'Content-Type': 'application/json' },
+			}
+		);
+	}
+	const [ productData, setProductData ] = useState({ ...product });
 
-  function updateData(value, field) {
-    setProductData({ ...productData, [field]: value });
-    console.log(value);
-  }
+	function updateData(value, field) {
+		setProductData({ ...productData, [field]: value });
+		console.log(value);
+	}
 
-  return (
-    <li className={currentProduct}>
-      {canEditProduct && (
-        <>
-          <Input
-            type="text"
-            labelText="Product Name:"
-            id="productName"
-            updateInfo={(e) => updateData(e.target.value, "productName")}
-            currentInfo={productData.productName}
-          />
-          <Input
-            type="number"
-            labelText="Product Price:"
-            id="productPrice"
-            updateInfo={(e) => updateData(e.target.value, "productPrice")}
-            currentInfo={productData.productPrice}
-          />
-          <Input
-            type="number"
-            labelText="Product Quantity:"
-            id="quantity"
-            updateInfo={(e) => updateData(e.target.value, "quantity")}
-            currentInfo={productData.quantity}
-          />
-        </>
-      )}
-      <span> {productData.productName}</span>
-      <span> £{productData.productPrice}</span>
-      <span> {productData.quantity}</span>
-      <Button
-        className="removeProduct"
-        textContent="remove"
-        onClick={() => {
-          removeProduct();
-          deleteProduct(product.productId);
-        }}
-      />
-      <Button
-        className="updateProduct"
-        textContent="edit"
-        onClick={() => {
-          toggleCanEdit();
-        }}
-      />
-      {canEditProduct && (
-        <Button
-          className="submitProduct"
-          textContent="submit"
-          onClick={() => {
-            editProduct(productData.productId);
-            toggleCanEdit();
-          }}
-        />
-      )}
-    </li>
-  );
+	return (
+		<li className={currentProduct}>
+      	<Button
+				className='updateProductEdit'
+				textContent='edit'
+				onClick={() => {
+					toggleCanEdit();
+				}}
+			/>
+
+			{canEditProduct ? (
+				<div className={currentProductForm}>
+					<Input
+						type='text'
+						labelText='Product Name:'
+						id='productName'
+						updateInfo={(e) =>
+							updateData(e.target.value, 'productName')}
+						currentInfo={productData.productName}
+					/>
+					<Input
+						type='number'
+						labelText='Product Price:'
+						id='productPrice'
+						updateInfo={(e) =>
+							updateData(e.target.value, 'productPrice')}
+						currentInfo={productData.productPrice}
+					/>
+					<Input
+						type='number'
+						labelText='Product Quantity:'
+						id='quantity'
+						updateInfo={(e) =>
+							updateData(e.target.value, 'quantity')}
+						currentInfo={productData.quantity}
+					/>
+					<Button
+						className='submitProduct'
+						textContent='submit'
+						onClick={() => {
+							editProduct(productData.productId);
+							toggleCanEdit();
+						}}
+					/>
+				</div>
+			) : (
+				<div className={currentProductInfo}>
+					<span> {productData.productName}</span>
+					<span> £{productData.productPrice}</span>
+					<span> {productData.quantity}</span>
+					<Button
+						className='removeCurrentProduct'
+						textContent='remove'
+						onClick={() => {
+							removeProduct();
+							deleteProduct(product.productId);
+						}}
+					/>
+				</div>
+			)}
+		</li>
+	);
 }
