0a9553b9a67dda07c5b50cbd86830877088d3726
Liam Morley <liammorley123@live.co.uk>
2021-04-03 17:41:39 +0100

    work on responsivness of the business profile fixing image bugs

--- src/components/BusinessProfile/index.js
+++ src/components/BusinessProfile/index.js
@@ -1,34 +1,33 @@
-import IsBusinessOpen from '../IsBusinessOpen';
+import IsBusinessOpen from "../IsBusinessOpen";
 import {
-	businessAbout,
-	businessImg,
-	businessName,
-	container,
-	contentContainer,
-} from './BusinessProfile.module.css';
+  businessAbout,
+  businessImg,
+  businessName,
+  container,
+  contentContainer,
+} from "./BusinessProfile.module.css";
 
 function BusinessProfile({ businessInfo }) {
-	//destructuring businessInfo object (which is our mockData) and displaying in on the page
-	
-	return (
-		
-		<div>{businessInfo && 
-		<div className={container}>
-			<IsBusinessOpen IsTrading={businessInfo.isTrading} />
-			<div className={contentContainer}>
-				<h2 className={businessName}>{businessInfo.businessName}</h2>
-				<p className={businessAbout}>{businessInfo.primaryEmail}</p>
+  //destructuring businessInfo object (which is our mockData) and displaying in on the page
 
-				<img
-					className={businessImg}
-					src={businessInfo.businessLogo}
-					alt="BusinessLogo"
-				/>
-			</div>
-		</div>}
-		</div>
-		);
-	
+  return (
+    <>
+      {businessInfo && (
+        <div className={container}>
+          <IsBusinessOpen IsTrading={businessInfo.isTrading} />
+          <div className={contentContainer}>
+            <h2 className={businessName}>{businessInfo.businessName}</h2>
+            <p className={businessAbout}>{businessInfo.primaryEmail}</p>
+            <img
+              className={businessImg}
+              src={businessInfo.businessLogo}
+              alt="BusinessLogo"
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default BusinessProfile;
