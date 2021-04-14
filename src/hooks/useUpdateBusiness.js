37e8f1f928971cdbd5c8be3d0de29dd66cc3c679
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-04-14 21:46:19 +0100

    useUpdateBusiness tests

--- src/hooks/useUpdateBusiness.js
+++ src/hooks/useUpdateBusiness.js
@@ -1,23 +1,23 @@
 function useUpdateBusiness(businessInfo, setBusinessInfo) {
 
-  async function updateData(newValue, property) {
+  function updateData(newValue, property) {
     setBusinessInfo({ ...businessInfo, [property]: newValue });
 
     //for demo
-    const { id } = businessInfo;
-    await fetch(
-      `${
-        process.env.REACT_APP_BACKEND_URL
-      }/business/updatebybusiness/${parseInt(id)}`,
-      {
-        method: "PUT",
-        body: JSON.stringify({
-          ...businessInfo,
-          id: businessInfo.id,
-        }),
-        headers: { "Content-Type": "application/json" },
-      }
-    );
+    // const { id } = businessInfo;
+    // await fetch(
+    //   `${
+    //     process.env.REACT_APP_BACKEND_URL
+    //   }/business/updatebybusiness/${parseInt(id)}`,
+    //   {
+    //     method: "PUT",
+    //     body: JSON.stringify({
+    //       ...businessInfo,
+    //       id: businessInfo.id,
+    //     }),
+    //     headers: { "Content-Type": "application/json" },
+    //   }
+    // );
     //
   }
 
--- src/hooks/useUpdateBusiness.test.js
+++ src/hooks/useUpdateBusiness.test.js
@@ -4,6 +4,50 @@ import useUpdateBusiness from './useUpdateBusiness';
 
 describe('useUpdateBusiness', () => {
 	afterEach(() => nock.cleanAll());
+	let businessInfo = { ...mockBusiness };
+	const setBusinessInfo = (newInfo) => (businessInfo = newInfo);
 
-	
+	const { updateData, updateBusiness } = useUpdateBusiness(
+		businessInfo,
+		setBusinessInfo
+	);
+
+	test('when given a value and a property, updateData updates the relative property in businessInfo', () => {
+		//Arrange
+		const expectedName = 'New Macrame Madness';
+		const expectedIsTrading = true;
+
+		//Act
+		updateData(expectedIsTrading, 'isTrading');
+		updateData(expectedName, 'businessName');
+
+		//Assert
+		expect(businessInfo.businessName).toBe(expectedName)
+		expect(businessInfo.isTrading).toBe(expectedIsTrading)
+	});
+
+	test('updateBusiness calls API and updates data for the business', async () => {
+		//Arrange
+		nock.disableNetConnect();
+		const scope = nock(process.env.REACT_APP_BACKEND_URL)
+			//CORS
+			.defaultReplyHeaders({
+				'access-control-allow-origin'      : '*',
+				'access-control-allow-credentials' : 'true',
+			})
+			.persist()
+			.intercept(
+				`/business/updatebybusiness/${businessInfo.id}`,
+				'OPTIONS'
+			)
+			.reply(200, null)
+			.put(`/business/updatebybusiness/${businessInfo.id}`)
+			.reply(200, { ...businessInfo });
+
+		//Act
+		await updateBusiness();
+
+		//Assert
+		expect(scope.isDone()).toBe(true);
+	});
 });
